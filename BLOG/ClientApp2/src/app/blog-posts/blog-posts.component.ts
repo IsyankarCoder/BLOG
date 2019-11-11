b7d2930b060084ddae5e78cffda1540be3439991
Volkan GENÇ <gencvolkan@gmail.com>
2019-11-11 10:10:22 -0800

    continueeeee

--- BLOG/ClientApp2/src/app/blog-posts/blog-posts.component.ts
+++ BLOG/ClientApp2/src/app/blog-posts/blog-posts.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogPostService } from '../blog-post.service';
+import { BlogPost } from '../../model/blogpost';
 
 @Component({
   selector: 'app-blog-posts',
   templateUrl: './blog-posts.component.html',
   styleUrls: ['./blog-posts.component.css']
 })
+
 export class BlogPostsComponent implements OnInit {
+  blogPosts$: Observable<BlogPost[]>;
 
-  constructor() { }
+  constructor(private blogPostService: BlogPostService)
+  {
 
-  ngOnInit() {
+  }
+
+  ngOnInit()
+  {
+    this.loadBlogPosts();
+  }
+
+  loadBlogPosts() {
+    this.blogPosts$ = this.blogPostService.getBlogPosts();
+  }
+
+  delete(postId) {
+    const ans = confirm('Do you want to delete blog post with id : ' + postId);
+    if (ans)
+      this.blogPostService.deleteBlogPost(postId).subscribe((data) => {
+        this.loadBlogPosts();
+      })
   }
 
 }
